c203e7271ff680a8c91bd1b140099a15b570a22d
Michael A Manzano <michael.a.manzano@intel.com>
2019-02-12 17:32:26 -0700

    loaders text

--- src/components/examples/Loaders.vue
+++ src/components/examples/Loaders.vue
@@ -2,23 +2,24 @@
 	.home
 		h1 Loaders
 		.message(style="text-align:left;")
-			| Indicating that 'something' is happening tends to be a problem in that its not
-			| always obvious that you are waiting because nothing conveys the issue.
+			| Indicating that network calls are being completed tends to be a action we poorly communicate. 
+			| Sometimes what starts as a quick call with practically 0 time taken later on blows up 
+			| into a long running action. Show a loader!
 		
 		ExampleLoading
 		ExampleLoadingArea
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import ExampleLoading from '@/components/examples/loading/ExampleLoading.vue';
-import ExampleLoadingArea from '@/components/examples/loading/ExampleLoadingArea.vue';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import ExampleLoading from "@/components/examples/loading/ExampleLoading.vue";
+import ExampleLoadingArea from "@/components/examples/loading/ExampleLoadingArea.vue";
 
 @Component({
 	components: {
 		ExampleLoading,
-		ExampleLoadingArea,
-	},
+		ExampleLoadingArea
+	}
 })
 export default class Loaders extends Vue {}
 </script>
--- src/components/examples/loading/ExampleLoading.vue
+++ src/components/examples/loading/ExampleLoading.vue
@@ -1,14 +1,18 @@
  <template lang="pug">
 	Example
 		template(slot='title')
-			| 1. Show an unobtrusive loader for actions
+			| 1. Let them know that something is happening
 		template(slot='good-reason')
 			b Good - 
-			span The button here has a loader indicator next to the input 
-			| which immediately appears and would remain until the action completes.
+			span After clicking the button you immediately get a response back 
+			| that something is happening as indicated by the spinning magnifying 
+			| glass. Not at least the user perceives that their request went somewhere.
 		template(slot='bad-reason')
-			b Not as good - 
-			span Upon clicking nothing happens
+			b Not so good - 
+			span After clicking the 'Find that thing!' button you would expect 
+			| some sort of waiting action to take place, but in this case 
+			| nothing happens, leaving you wondering if something broke or 
+			| you should wait forever.
 		template(slot='good-content')
 			form#example.simple-form(v-on:submit.prevent)
 				fieldset.simple-form__fieldset
@@ -37,13 +41,13 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import Example from '@/components/examples/Example.vue';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Example from "@/components/examples/Example.vue";
 
 @Component({
 	components: {
-		Example,
-	},
+		Example
+	}
 })
 export default class ExampleLoading extends Vue {
 	private isLoading: boolean = false;
@@ -58,8 +62,8 @@ export default class ExampleLoading extends Vue {
 </script>
 
 <style lang="scss" scoped>
-@import 'src/toolkit.scss';
-@import 'src/variables.scss';
+@import "src/toolkit.scss";
+@import "src/variables.scss";
 
 #example {
 	@include simple-form();
